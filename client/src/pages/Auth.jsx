475e4914cc20066de169a157341f129a7b16df27
Shubham Kumar <shubhamkumar7549yuv@gmail.com>
2026-05-27 20:00:20 +0530

    Fix refresh

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -107,7 +107,7 @@ function Auth({isModel = false}) {
                     <BsRobot size={18}/>
 
                 </div>
-                <h2 className='font-semibold text-lg'>InterviewIQ.AI</h2>
+                <h2 className='font-semibold text-lg'>MockVerse.Online</h2>
             </div>
 
             <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>
